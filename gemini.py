7b2b9c6e501abb44b33357e276c0df9402193919
andrei12ai <111877984+andrei12ai@users.noreply.github.com>
2024-11-14 14:22:15 +0200

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -19,7 +19,7 @@ model = genai.GenerativeModel(
   system_instruction="You are an assistant for industrial workflow processing. You will be given a JSON file representing a detailed configuration of a workflow system. The system is composed of steps that interact with other steps through defined inputs, outputs, and conditions. Your job is to analyze such workflows based on user prompts and provide specific information and visualization, modify workflows according to user instructions and/or generate new workflows following the structure presented in the JSON file. ",
 )
 
-chat_session = model.start_chat(
+chat = model.start_chat(
   history=[
     {
       "role": "user",
@@ -35,7 +35,40 @@ chat_session = model.start_chat(
     },
   ]
 )
-prompt = st.chat_input("Hello")
-response = chat_session.send_message("Hello")
+with st.sidebar:
+    if st.button("Clear Chat Window", use_container_width=True, type="primary"):
+        st.session_state.history = []
+        st.rerun()
 
-st.write(response.text)
+for message in chat.history:
+    role ="assistant" if message.role == 'model' else message.role
+    with st.chat_message(role):
+        st.markdown(message.parts[0].text)
+
+if "app_key" in st.session_state:
+    if prompt := st.chat_input(""):
+        prompt = prompt.replace('\n', ' \n')
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            message_placeholder.markdown("Thinking...")
+            try:
+                full_response = ""
+                for chunk in chat.send_message(prompt, stream=True):
+                    word_count = 0
+                    random_int = random.randint(5,10)
+                    for word in chunk.text:
+                        full_response+=word
+                        word_count+=1
+                        if word_count == random_int:
+                            time.sleep(0.05)
+                            message_placeholder.markdown(full_response + "_")
+                            word_count = 0
+                            random_int = random.randint(5,10)
+                message_placeholder.markdown(full_response)
+            except genai.types.generation_types.BlockedPromptException as e:
+                st.exception(e)
+            except Exception as e:
+                st.exception(e)
+            st.session_state.history = chat.history
